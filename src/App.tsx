38a842219c737e5ed3abf2df8c1074c2884c9d7f
nathc <nathc@udel.edu>
2025-02-25 16:32:12 -0500

    Added font change and folder tabs

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <div className="Clock">12:00:00</div>
+            <div className="upper-tab-container">
+                <div className="Starter-Tab-Button"></div>
+                <div className="follower-button-container">
+                    <div className="Follower-Tab-Button"></div>
+                    <div className="Follower-Tab-Button-Inner"></div>
+                </div>
+                <div className="follower-button-container">
+                    <div className="Follower-Tab-Button"></div>
+                    <div className="Follower-Tab-Button-Inner"></div>
+                </div>
+            </div>
             <header className="App-header">Nathan C</header>
         </div>
     );
