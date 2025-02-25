07241bb2337170ae7e4a18f573fa2eebf2109198
nathc <nathc@udel.edu>
2025-02-24 21:53:38 -0500

    added clock and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,10 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <div className="Clock">12:00</div>
+            <header className="App-header">Nathan C</header>
         </div>
     );
 }
-
+//Edit src/App.tsx and save. This page will automatically reload.
 export default App;
