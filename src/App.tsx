4986e325bb77eb9929be127a77d2d351abfb981f
nathc657 <nathc@udel.edu>
2025-02-26 17:43:33 -0500

    changing caps and tab color

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,21 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <div className="Clock">12:00:00</div>
+
             <div className="upper-tab-container">
-                <div className="Starter-Tab-Button"></div>
-                <div className="follower-button-container">
-                    <div className="Follower-Tab-Button"></div>
-                    <div className="Follower-Tab-Button-Inner"></div>
+                <div className="starter-button-container">
+                    <div className="starter-tab-button-outer"></div>
+                    <div className="starter-tab-button-inner"></div>
                 </div>
+
                 <div className="follower-button-container">
-                    <div className="Follower-Tab-Button"></div>
-                    <div className="Follower-Tab-Button-Inner"></div>
+                    <div className="follower-tab-button-outer"></div>
+                    <div className="follower-tab-button-inner"></div>
+                </div>
+
+                <div className="follower-button-container">
+                    <div className="follower-tab-button-outer"></div>
+                    <div className="follower-tab-button-inner"></div>
                 </div>
             </div>
             <header className="App-header">Nathan C</header>
