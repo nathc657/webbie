8321f6adc948c6b7e6e321e03e16421dea7c516c
nathc657 <nathc@udel.edu>
2025-02-27 14:42:15 -0500

    Added selecting tabs feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App(): JSX.Element {
+    const [selectedTab, setSelectedTab] = useState(1);
+
+    const selectTab = (tabNumber: number) => {
+        setSelectedTab(tabNumber);
+    };
+
     return (
         <div className="App">
             <div className="Clock">12:00:00</div>
@@ -9,22 +15,59 @@ function App(): JSX.Element {
             <div className="upper-tab-container">
                 <div className="starter-shell-container">
                     <div className="starter-tab-shell-outer"></div>
-                    <div className="starter-tab-shell-inner"></div>
+                    <div
+                        className="starter-tab-shell-inner"
+                        //If selected tab === 1, set to white, else black.
+                        style={{
+                            backgroundColor:
+                                selectedTab === 1 ? "white" : "black"
+                        }}
+                    ></div>
+                    <div
+                        className="starter-tab-button"
+                        onClick={() => selectTab(1)}
+                    >
+                        Button
+                    </div>
                 </div>
 
                 <div className="follower-shell-container">
                     <div className="follower-tab-shell-outer"></div>
-                    <div className="follower-tab-shell-inner"></div>
+                    <div
+                        className="follower-tab-shell-inner"
+                        style={{
+                            backgroundColor:
+                                selectedTab === 2 ? "white" : "black"
+                        }}
+                    ></div>
+                    <div
+                        className="follower-tab-button"
+                        onClick={() => selectTab(2)}
+                    >
+                        Button
+                    </div>
                 </div>
 
                 <div className="follower-shell-container">
                     <div className="follower-tab-shell-outer"></div>
-                    <div className="follower-tab-shell-inner"></div>
+                    <div
+                        className="follower-tab-shell-inner"
+                        style={{
+                            backgroundColor:
+                                selectedTab === 3 ? "white" : "black"
+                        }}
+                    ></div>
+                    <div
+                        className="follower-tab-button"
+                        onClick={() => selectTab(3)}
+                    >
+                        Button
+                    </div>
                 </div>
             </div>
             <header className="App-header">Nathan C</header>
         </div>
     );
 }
-//Edit src/App.tsx and save. This page will automatically reload.
+
 export default App;
