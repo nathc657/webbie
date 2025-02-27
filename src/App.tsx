b84fc9d8deea5eb295fd030be66c8ca6be2b669d
nathc657 <nathc@udel.edu>
2025-02-26 19:41:13 -0500

    changes var names

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ function App(): JSX.Element {
             <div className="Clock">12:00:00</div>
 
             <div className="upper-tab-container">
-                <div className="starter-button-container">
-                    <div className="starter-tab-button-outer"></div>
-                    <div className="starter-tab-button-inner"></div>
+                <div className="starter-shell-container">
+                    <div className="starter-tab-shell-outer"></div>
+                    <div className="starter-tab-shell-inner"></div>
                 </div>
 
-                <div className="follower-button-container">
-                    <div className="follower-tab-button-outer"></div>
-                    <div className="follower-tab-button-inner"></div>
+                <div className="follower-shell-container">
+                    <div className="follower-tab-shell-outer"></div>
+                    <div className="follower-tab-shell-inner"></div>
                 </div>
 
-                <div className="follower-button-container">
-                    <div className="follower-tab-button-outer"></div>
-                    <div className="follower-tab-button-inner"></div>
+                <div className="follower-shell-container">
+                    <div className="follower-tab-shell-outer"></div>
+                    <div className="follower-tab-shell-inner"></div>
                 </div>
             </div>
             <header className="App-header">Nathan C</header>
